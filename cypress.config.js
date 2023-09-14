76374ac698563ae90be22ad656374348acaa5e2b
sariwahyunita <wahyunitas@gmail.com>
2023-09-14 16:21:48 +0700

    test

--- cypress.config.js
+++ cypress.config.js
@@ -7,15 +7,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on('before:run', async (details) => {
-        console.log('override before:run');
-        await beforeRunHook(details);
-      });
-
-      on('after:run', async () => {
-        console.log('override after:run');
-        await afterRunHook();
-      });
     },
   },
 });
